b3b84e9f9de5c42c26adf2b8fde2fd159db2e009
giosroom99 <giovannitshibangu@icloud.com>
2023-12-01 14:32:15 -0500

    working ?

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -5,7 +5,7 @@ import Better from './BetterWalkReport';
 function Main() {
   return (
     <div className="">
-      <div className="row mt-3">
+      <div className="row my-4 py-4">
         <h2 className="text-center fw-bold">Better Walk Report</h2>
       </div>
       <div className="row">
